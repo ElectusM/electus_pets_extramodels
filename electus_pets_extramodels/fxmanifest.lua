623610f90ad62e82d2da0b7fcb543863329af2b6
ElectusM <145289160+ElectusM@users.noreply.github.com>
2024-06-28 15:17:46 +0200

    Initial

---
+++ electus_pets_extramodels/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version "cerulean"
+author "krillee1 (discord) (ELECTUS SCRIPTS)"
+fx_version 'cerulean'
+lua54 'yes'
+game 'gta5'
+
+files {
+    'peds.meta',
+    'stream/*.ydd',
+    'stream/*.yft',
+    'stream/*.ymt',
+    'stream/*.ytd',
+    'stream/*.ydr',
+}
+
+data_file 'PED_METADATA_FILE' 'peds.meta'
